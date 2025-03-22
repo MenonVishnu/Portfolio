1ac7ecef3599ebd84223199fe820cc44e5111b3f
Vishnu Menon <menonvishnu26@gmail.com>
2025-03-22 21:59:28 +0530

    Changed Project data to array

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,93 +5,88 @@ import ProjImage1 from "../assets/dummyproject-1.jpg";
 import ProjImage2 from "../assets/dummyproject-2.jpg";
 import ProjectCard from "./ProjectCard";
 
-var Project1 = {
-	image: ProjImage1,
-	date: "June 2024",
-	link: "https://github.com/MenonVishnu/BudgetApplication",
-	title: "Budget Application API",
-	tech: "Golang, Gorilla/Mux",
-	description:
-		"Developed a daily budgeting app using GoLang and Gorilla/mux. Features CRUD operations, user/admin routes, and seamless MongoDB integration.",
-};
+const projects = [
+	{
+		image: ProjImage1,
+		date: "July 2022",
+		link: "https://github.com/MenonVishnu/OTT-Platform-Analysis-Tool",
+		title: "OTT Platform Analysis",
+		tech: "Python, QlikSense",
+		description:
+			"Data analysis between Tv Shows & Movies across different OTT Platforms (Disney Hotstar, Netflix, Amazon Prime)",
+	},
+	{
+		image: ProjImage1,
+		date: "October 2022",
+		link: "https://github.com/MenonVishnu/TShirtStore-API",
+		title: "T-Shirt Store API",
+		tech: "Node, Express, MongoDB",
+		description:
+			"Created an e-commerce API with Node.js and MongoDB, implementing JWT-based authentication, CRUD operations, RazorPay payment integration, and Cloudinary photo storage.",
+	},
+	{
+		image: ProjImage2,
+		date: "November 2022",
+		link: "https://github.com/MenonVishnu/NoteTakingApplication-API",
+		title: "Note Taking Application - API",
+		tech: "Node, Express, MongoDB",
+		description: "A basic Note taking Application.",
+	},
+	{
+		image: ProjImage2,
+		date: "March 2023",
+		link: "https://github.com/dipeshsanil/CUB",
+		title: "Cyberlocker using Blockchain",
+		tech: "React.js, Solidity, IPFS",
+		description:
+			"Built a decentralized file storage system using ReactJS, Solidity, and IPFS. Enabled secure file sharing through smart contracts.",
+	},
+	{
+		image: ProjImage1,
+		date: "June 2024",
+		link: "https://github.com/MenonVishnu/BudgetApplication",
+		title: "Budget Application API",
+		tech: "Golang, Gorilla/Mux",
+		description:
+			"Developed a daily budgeting app using GoLang and Gorilla/mux. Features CRUD operations, user/admin routes, and seamless MongoDB integration.",
+	},
+	{
+		image: ProjImage1,
+		date: "October 2024",
+		link: "https://github.com/MenonVishnu",
+		title: "LegalSetu",
+		tech: "Python, Chainlit",
+		description: "A RAG based Legal AI Assistant",
+	},
 
-var Project2 = {
-	image: ProjImage2,
-	date: "March 2023",
-	link: "https://github.com/dipeshsanil/CUB",
-	title: "Cyberlocker using Blockchain",
-	tech: "React.js, Solidity, IPFS",
-	description:
-		"Built a decentralized file storage system using ReactJS, Solidity, and IPFS. Enabled secure file sharing through smart contracts.",
-};
-
-var Project3 = {
-	image: ProjImage1,
-	date: "October 2022",
-	link: "https://github.com/MenonVishnu/TShirtStore-API",
-	title: "T-Shirt Store API",
-	tech: "Node, Express, MongoDB",
-	description:
-		"Created an e-commerce API with Node.js and MongoDB, implementing JWT-based authentication, CRUD operations, RazorPay payment integration, and Cloudinary photo storage.",
-};
-
-var Project4 = {
-	image: ProjImage2,
-	date: "January 2025",
-	link: "https://github.com/MenonVishnu/Portfolio",
-	title: "Portfolio Website",
-	tech: "React.js, HTML, CSS",
-	description:
-		"A Simple static portfolio website showcasing my Projects, Experience & Skills. ",
-};
-
-var Project5 = {
-	image: ProjImage1,
-	date: "October 2024",
-	link: "https://github.com/MenonVishnu",
-	title: "LegalSetu",
-	tech: "Python, Chainlit",
-	description: "A RAG based Legal AI Assistant",
-};
-
-var Project6 = {
-	image: ProjImage2,
-	date: "November 2022",
-	link: "https://github.com/MenonVishnu/NoteTakingApplication-API",
-	title: "Note Taking Application - API",
-	tech: "Node, Express, MongoDB",
-	description: "A basic Note taking Application.",
-};
-
-var Project7 = {
-	image: ProjImage1,
-	date: "July 2022",
-	link: "https://github.com/MenonVishnu/OTT-Platform-Analysis-Tool",
-	title: "OTT Platform Analysis",
-	tech: "Python, QlikSense",
-	description:
-		"Data analysis between Tv Shows & Movies across different OTT Platforms (Disney Hotstar, Netflix, Amazon Prime)",
-};
-
-var Project8 = {
-	image: ProjImage1,
-	date: "December 2024",
-	link: "https://github.com/MenonVishnu/Go-URLShortner",
-	title: "Go-URL Shortner",
-	tech: "Golang, Go-Fiber, Redis, Docker, Docker-Compose",
-	description:
-		"API which allows user to redirect from shortened URL to the actual URL",
-};
-
-var Project9 = {
-	image: ProjImage2,
-	date: "January 2025",
-	link: "https://github.com/MenonVishnu/Go-WeatherReport",
-	title: "Go-Weather Report",
-	tech: "Golang, SMTP, CRON, Redis, Docker-Compose",
-	description:
-		"API which allows user to get weather report of the city they specify",
-};
+	{
+		image: ProjImage2,
+		date: "January 2025",
+		link: "https://github.com/MenonVishnu/Go-WeatherReport",
+		title: "Go-Weather Report",
+		tech: "Golang, SMTP, CRON, Redis, Docker-Compose",
+		description:
+			"API which allows user to get weather report of the city they specify",
+	},
+	{
+		image: ProjImage1,
+		date: "December 2024",
+		link: "https://github.com/MenonVishnu/Go-URLShortner",
+		title: "Go-URL Shortner",
+		tech: "Golang, Go-Fiber, Redis, Docker, Docker-Compose",
+		description:
+			"API which allows user to redirect from shortened URL to the actual URL",
+	},
+	{
+		image: ProjImage2,
+		date: "January 2025",
+		link: "https://github.com/MenonVishnu/Portfolio",
+		title: "Portfolio Website",
+		tech: "React.js, HTML, CSS",
+		description:
+			"A Simple static portfolio website showcasing my Projects, Experience & Skills. ",
+	},
+];
 
 function Projects() {
 	return (

50551cf5a591458c3809e4fbf54ab35eea77b68d
Vishnu Menon <menonvishnu26@gmail.com>
2025-03-26 16:27:14 +0530

    Partial designing of Modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,26 +5,37 @@ import "./Modal.css";
 const overlayStyle = { background: "rgba(0,0,0,0.5)" };
 
 const Modal = (props) => (
-	<Popup
-		trigger={<button className="button">Details</button>}
-		modal
-		{...{ overlayStyle }}>
-		{(close) => (
-			<div className="modal-container">
-				Popup content here !!
-				<button className="close-button" onClick={() => close()}>
-					X
-				</button>
-				<div class="modal-title">Project Title</div>
-				<div class="architecture">
-					<img src="architecture.jpg" alt="Architecture" />
-				</div>
-				<div class="tech-stack">{props.data.tech}</div>
-				<div class="modal-description">{props.data.description}</div>
-				<button onClick={() => close()}>Close</button>
-			</div>
-		)}
-	</Popup>
+  <Popup
+    trigger={<button className="button">Details</button>}
+    modal
+    {...{ overlayStyle }}
+  >
+    {(close) => (
+      <div className="modal-container">
+        <div className="modal-inside-container">
+          <div className="x-button" onClick={() => close()}>
+            x
+          </div>
+          <div class="modal-title">{props.data.title}</div>
+        </div>
+        <div className="content-container">
+          <div className="left">
+            <img
+              class="architecture"
+              src={props.data.architecture}
+              alt="Architecture"
+            />
+          </div>
+          <div className="center"></div>
+          <div className="right">
+            <div class="tech-stack">{props.data.tech}</div>
+            <div class="modal-description">{props.data.description}</div>
+          </div>
+        </div>
+        <button onClick={() => close()}>Close</button>
+      </div>
+    )}
+  </Popup>
 );
 
 export default Modal;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,127 +22,141 @@ import CSS from "../assets/Icons/css.svg";
 import Mux from "../assets/Icons/mux.png";
 import Chainlit from "../assets/Icons/chainlit.png";
 
+//Import Architecture
+import Architecture_9 from "../assets/ProjectArchitecture/goweather.svg";
+
 const ProjectData = [
-	{
-		id: 1,
-		image: ProjImage1,
-		date: "July 2022",
-		link: "https://github.com/MenonVishnu/OTT-Platform-Analysis-Tool",
-		title: "OTT Platform Analysis",
-		tech: "Python, QlikSense",
-		techIcon: [Python, Qlik],
-		description:
-			"Data analysis between Tv Shows & Movies across different OTT Platforms (Disney Hotstar, Netflix, Amazon Prime)",
-	},
-	{
-		id: 2,
-		image: ProjImage2,
-		date: "October 2022",
-		link: "https://github.com/MenonVishnu/TShirtStore-API",
-		title: "T-Shirt Store API",
-		tech: "Node, Express, MongoDB",
-		techIcon: [Node, Express, Mongo],
-		description:
-			"Created an e-commerce API with Node.js and MongoDB, implementing JWT-based authentication, CRUD operations, RazorPay payment integration, and Cloudinary photo storage.",
-	},
-	{
-		id: 3,
-		image: ProjImage1,
-		date: "November 2022",
-		link: "https://github.com/MenonVishnu/NoteTakingApplication-API",
-		title: "Note Taking Application - API",
-		tech: "Node, Express, MongoDB",
-		techIcon: [Node, Express, Mongo],
-		description: "A basic Note taking Application.",
-	},
-	{
-		id: 4,
-		image: ProjImage2,
-		date: "March 2023",
-		link: "https://github.com/dipeshsanil/CUB",
-		title: "Cyberlocker using Blockchain",
-		tech: "React.js, Solidity, IPFS",
-		techIcon: [ReactJS, Solidity, IPFS],
-		description:
-			"Built a decentralized file storage system using ReactJS, Solidity, and IPFS. Enabled secure file sharing through smart contracts.",
-	},
-	{
-		id: 5,
-		image: ProjImage1,
-		date: "June 2024",
-		link: "https://github.com/MenonVishnu/BudgetApplication",
-		title: "Budget Application API",
-		tech: "Golang, Gorilla/Mux",
-		techIcon: [Go, Mux],
-		description:
-			"Developed a daily budgeting app using GoLang and Gorilla/mux. Features CRUD operations, user/admin routes, and seamless MongoDB integration.",
-	},
-	{
-		id: 6,
-		image: ProjImage2,
-		date: "October 2024",
-		link: "https://github.com/MenonVishnu",
-		title: "LegalSetu",
-		tech: "Python, Chainlit",
-		techIcon: [Python, Chainlit],
-		description: "A RAG based Legal AI Assistant",
-	},
-	{
-		id: 7,
-		image: ProjImage1,
-		date: "December 2024",
-		link: "https://github.com/MenonVishnu/Go-URLShortner",
-		title: "Go-URL Shortner",
-		tech: "Golang, Go-Fiber, Redis, Docker, Docker-Compose",
-		techIcon: [Go, Redis, Docker],
-		description:
-			"API which allows user to redirect from shortened URL to the actual URL",
-	},
-	{
-		id: 8,
-		image: ProjImage2,
-		date: "January 2025",
-		link: "https://github.com/MenonVishnu/Portfolio",
-		title: "Portfolio Website",
-		tech: "React.js, HTML, CSS",
-		techIcon: [ReactJS, HTML, CSS],
-		description:
-			"A Simple static portfolio website showcasing my Projects, Experience & Skills. ",
-	},
-	{
-		id: 9,
-		image: ProjImage1,
-		date: "January 2025",
-		link: "https://github.com/MenonVishnu/Go-WeatherReport",
-		title: "Go-Weather Report",
-		tech: "Golang, SMTP, CRON, Redis, Docker-Compose",
-		techIcon: [Go, Redis, Docker],
-		description:
-			"API which allows user to get weather report of the city they specify",
-	},
-	//Add new projects here, Also consider above projects for image number
+  {
+    id: 1,
+    image: ProjImage1,
+    date: "July 2022",
+    link: "https://github.com/MenonVishnu/OTT-Platform-Analysis-Tool",
+    title: "OTT Platform Analysis",
+    tech: "Python, QlikSense",
+    techIcon: [Python, Qlik],
+    shortDescription: "",
+    description:
+      "Data analysis between Tv Shows & Movies across different OTT Platforms (Disney Hotstar, Netflix, Amazon Prime)",
+    architecture: "",
+  },
+  {
+    id: 2,
+    image: ProjImage2,
+    date: "October 2022",
+    link: "https://github.com/MenonVishnu/TShirtStore-API",
+    title: "T-Shirt Store API",
+    tech: "Node, Express, MongoDB",
+    techIcon: [Node, Express, Mongo],
+    shortDescription: "",
+    description:
+      "Created an e-commerce API with Node.js and MongoDB, implementing JWT-based authentication, CRUD operations, RazorPay payment integration, and Cloudinary photo storage.",
+  },
+  {
+    id: 3,
+    image: ProjImage1,
+    date: "November 2022",
+    link: "https://github.com/MenonVishnu/NoteTakingApplication-API",
+    title: "Note Taking Application - API",
+    tech: "Node, Express, MongoDB",
+    techIcon: [Node, Express, Mongo],
+    shortDescription: "",
+    description: "A basic Note taking Application.",
+  },
+  {
+    id: 4,
+    image: ProjImage2,
+    date: "March 2023",
+    link: "https://github.com/dipeshsanil/CUB",
+    title: "Cyberlocker using Blockchain",
+    tech: "React.js, Solidity, IPFS",
+    techIcon: [ReactJS, Solidity, IPFS],
+    shortDescription: "",
+    description:
+      "Built a decentralized file storage system using ReactJS, Solidity, and IPFS. Enabled secure file sharing through smart contracts.",
+  },
+  {
+    id: 5,
+    image: ProjImage1,
+    date: "June 2024",
+    link: "https://github.com/MenonVishnu/BudgetApplication",
+    title: "Budget Application API",
+    tech: "Golang, Gorilla/Mux",
+    techIcon: [Go, Mux],
+    shortDescription: "",
+    description:
+      "Developed a daily budgeting app using GoLang and Gorilla/mux. Features CRUD operations, user/admin routes, and seamless MongoDB integration.",
+  },
+  {
+    id: 6,
+    image: ProjImage2,
+    date: "October 2024",
+    link: "https://github.com/MenonVishnu",
+    title: "LegalSetu",
+    tech: "Python, Chainlit",
+    techIcon: [Python, Chainlit],
+    shortDescription: "",
+    description: "A RAG based Legal AI Assistant",
+  },
+  {
+    id: 7,
+    image: ProjImage1,
+    date: "December 2024",
+    link: "https://github.com/MenonVishnu/Go-URLShortner",
+    title: "Go-URL Shortner",
+    tech: "Golang, Go-Fiber, Redis, Docker, Docker-Compose",
+    techIcon: [Go, Redis, Docker],
+    shortDescription: "",
+    description:
+      "API which allows user to redirect from shortened URL to the actual URL",
+  },
+  {
+    id: 8,
+    image: ProjImage2,
+    date: "January 2025",
+    link: "https://github.com/MenonVishnu/Portfolio",
+    title: "Portfolio Website",
+    tech: "React.js, HTML, CSS",
+    techIcon: [ReactJS, HTML, CSS],
+    shortDescription: "",
+    description:
+      "A Simple static portfolio website showcasing my Projects, Experience & Skills. ",
+  },
+  {
+    id: 9,
+    image: ProjImage1,
+    date: "January 2025",
+    link: "https://github.com/MenonVishnu/Go-WeatherReport",
+    title: "Go-Weather Report",
+    tech: "Golang, SMTP, CRON, Redis, Docker-Compose",
+    techIcon: [Go, Redis, Docker],
+    shortDescription: "",
+    description:
+      "API which allows user to get weather report of the city they specify",
+    architecture: Architecture_9,
+  },
+  //Add new projects here, Also consider above projects for image number
 ];
 
 function Projects() {
-	return (
-		<section id="project">
-			<h2>projects.</h2>
-			<p class="description">
-				Explore some of the exciting projects I’ve worked on, showcasing my
-				expertise in backend development, API creation, and blockchain
-				integration. Each project reflects my problem-solving skills and passion
-				for developing efficient, user-focused solutions.
-			</p>
+  return (
+    <section id="project">
+      <h2>projects.</h2>
+      <p class="description">
+        Explore some of the exciting projects I’ve worked on, showcasing my
+        expertise in backend development, API creation, and blockchain
+        integration. Each project reflects my problem-solving skills and passion
+        for developing efficient, user-focused solutions.
+      </p>
 
-			<div class="case-studies">
-				{ProjectData.slice()
-					.reverse()
-					.map((project) => {
-						return <ProjectCard data={project} key={project.id} />;
-					})}
-			</div>
-		</section>
-	);
+      <div class="case-studies">
+        {ProjectData.slice()
+          .reverse()
+          .map((project) => {
+            return <ProjectCard data={project} key={project.id} />;
+          })}
+      </div>
+    </section>
+  );
 }
 
 export default Projects;

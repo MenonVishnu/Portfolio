13114c0d8a1125e430f15bdc09623aa7b16df77c
Vishnu Menon <menonvishnu26@gmail.com>
2025-03-26 10:42:29 +0530

    Changed title format in Project Card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,41 +1,43 @@
 import React from "react";
 import Modal from "./Modal/Modal";
-import "../App.css";
+import "./ProjectCard.css";
 
 function ProjectCard(props) {
-	return (
-		<div className="case-study">
-			<img src={props.data.image} alt="Case Study 1" />
-			<div className="project-link">
-				{/* github link first or date first? */}
-				<i>
-					<a href={props.data.link} target="_blank">
-						Github
-					</a>
-				</i>
-				<p className="date">{props.data.date}</p>
-			</div>
+  return (
+    <div className="case-study">
+      <div className="title-container">
+        <h3>{props.data.title}</h3>
+        <img src={props.data.image} alt="Case Study 1" />
+      </div>
+      <div className="project-link">
+        {/* github link first or date first? */}
+        <i>
+          <a href={props.data.link} target="_blank">
+            Github
+          </a>
+        </i>
+        <p className="date">{props.data.date}</p>
+      </div>
 
-			<h3>{props.data.title}</h3>
-			<h4>
-				<b>Tech Stack:</b>{" "}
-				{props.data.techIcon.map((icon, index) => {
-					return (
-						<img
-							className="tech-icon"
-							src={icon}
-							key={index}
-							style={{ zIndex: props.data.techIcon.length - index }}
-						/>
-					);
-				})}
-			</h4>
-			{/* <p>{props.data.description}</p> */}
-			<div class="button-container">
-				<Modal data={props.data} />
-			</div>
-		</div>
-	);
+      <h4>
+        <b>Tech Stack:</b>{" "}
+        {props.data.techIcon.map((icon, index) => {
+          return (
+            <img
+              className="tech-icon"
+              src={icon}
+              key={index}
+              style={{ zIndex: props.data.techIcon.length - index }}
+            />
+          );
+        })}
+      </h4>
+      <p>{props.data.description}</p>
+      <div class="button-container">
+        <Modal data={props.data} />
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
